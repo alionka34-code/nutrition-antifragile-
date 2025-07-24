f9cfed9427dc4ec64d2215e5a8b9e456c29a7740
Jarod Thongkhamdy <jarod@mac.home>
2025-07-24 12:18:25 +0200

    fetch articles

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,7 +3,7 @@ const API_URL = process.env.REACT_APP_API_URL;
 export async function fetchArticles() {
   const res = await fetch(`${API_URL}/articles/`);
   if (!res.ok) {
-    throw new Error('Failed to fetch articles ${res.status}`);');
+    throw new Error(`Erreur ${res.status}`);
   }
   return await res.json();
 }
