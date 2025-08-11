8e49bbfca9dcbbb607612cd7968b22832fb2af3e
Jarod Thongkhamdy <jarod@mac-mini-de-jarod.home>
2025-08-11 23:55:10 +0200

    favicon

--- frontend/src/pages/SubscriptionCancel.jsx
+++ frontend/src/pages/SubscriptionCancel.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function SubscriptionCancel() {
   return (
-    <div className="container">
-      <h1>Paiement annulé ❌</h1>
-      <p>Votre paiement n’a pas été finalisé. Vous pouvez réessayer.</p>
-      <a href="/abonnement">Réessayer</a>
+    <>
+     <header className='pt-10 bg-linear-to-t from-white to-gray-200'></header>
+    <div className="border-2 border-marron rounded-2xl mx-4 md:mx-auto md:max-w-5xl p-6 text-center my-10">
+      <h1 className = "font-SFBold text-marron text-4xl md:text-6xl">Paiement annulé ❌</h1>
+      <p className = "font-SF text-xl md:text-2xl mt-2">Votre paiement n’a pas été finalisé. Vous pouvez réessayer.</p>
+      <Link to="/abonnement" className = "font-SF text-xl md:text-2xl mt-2">Réessayer</Link>
     </div>
+    </>
   );
 }
--- frontend/src/pages/SubscriptionSuccess.jsx
+++ frontend/src/pages/SubscriptionSuccess.jsx
@@ -1,15 +1,19 @@
 import React, { useContext } from 'react';
 import { AuthContext } from "../contexts/AuthContextDefinition";
+import { Link } from 'react-router-dom';
 
 export default function SubscriptionSuccess() {
   const { username } = useContext(AuthContext);
 
   return (
-    <div className="container">
-      <h1>Paiement réussi 🎉</h1>
-      <p>Merci pour votre abonnement !</p>
+    <>
+    <header className='pt-10 bg-linear-to-t from-white to-gray-200'></header>
+    <div className="border-2 border-marron rounded-2xl mx-4 md:mx-auto md:max-w-5xl p-6 text-center my-10">
+      <h1 className = "font-SFBold text-marron text-4xl md:text-6xl">PAIEMENT REUSSI ! 🎉</h1>
+      <p className = "font-SF text-xl md:text-2xl mt-2">Merci pour votre abonnement !</p>
       {username && <p>Bienvenue, {username} !</p>}
-      <a href="/articles">Accéder aux articles premium</a>
+      <Link to="/articles" className='font-SF text-xl md:text-2xl mt-2'>Tu peux explorer l’ensemble de nos articles : enquêtes, réflexions et analyses exclusives.</Link>
     </div>
+    </>
   );
 }
