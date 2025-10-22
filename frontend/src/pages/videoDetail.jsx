f9fab12e9abfe5d98a7cbf3aed470dbd581213b3
Jarod Thongkhamdy <thongkhamdyjarod@gmail.com>
2025-10-22 22:08:43 +0200

    change overlay padding

--- frontend/src/pages/videoDetail.jsx
+++ frontend/src/pages/videoDetail.jsx
@@ -133,7 +133,7 @@ function VideoDetail() {
               
               {/* Overlay avec message d'abonnement */}
               <div className="absolute inset-0 flex flex-col items-center justify-center">
-                <div className="text-center p-3 md:p-8 bg-white rounded-xl shadow-2xl max-w-xs md:max-w-xl mx-3 md:mx-4">
+                <div className="text-center  p-2 md:p-8 bg-white rounded-xl shadow-2xl max-w-xs md:max-w-xl mx-3 md:mx-4">
                   <div className="mb-3 md:mb-6">
                     <svg className="w-8 h-8 md:w-16 md:h-16 text-marron mx-auto mb-2 md:mb-4" fill="currentColor" viewBox="0 0 20 20">
                       <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
