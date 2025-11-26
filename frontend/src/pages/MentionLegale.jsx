a4e7848c702b659ef1659e825dbd6b124e1036da
alionka34-code <alionka.houl34@gmail.com>
2025-11-26 11:45:50 +0100

    Fix typographical errors in MentionLegale.jsx

--- frontend/src/pages/MentionLegale.jsx
+++ frontend/src/pages/MentionLegale.jsx
@@ -34,7 +34,7 @@ function MentionLegale() {
             <h1 className="text-2xl font-SFBold mb-4 ">Propriété intellectuelle</h1>
             <p className="font-SF mb-4">L’ensemble des contenus de ce site (textes, images, vidéos, extraits d’œuvres, publications) relève de la propriété exclusive de l’auteure, sauf mention contraire.<br/> Toute reproduction, représentation, adaptation ou diffusion, intégrale ou partielle, sans autorisation écrite préalable, est interdite conformément aux articles L.111-1 et s. du Code de la propriété intellectuelle.</p>
             <h1 className="text-2xl font-SFBold mb-4">Gestion, diffusion et distribution</h1>
-            <p className="font-SF mb-4">La gestion administrative, la diffusion et la distribution des œuvres de l’auteure sont assurées, sous mandat exclusif, par Éditions Œuvres — Symbiose Audiovisuelle, dans le cadre de ses statuts et de ses accords de gestion des droits d’auteur.<br/><br/>
+            <p className="font-SF mb-4">La gestion administrative, la diffusion et la distribution des œuvres de l’auteure sont assurées, sous mandat exclusif, par Editions Oeuvres — Symbiose Audiovisuelle, dans le cadre de ses statuts et de ses accords de gestion des droits d’auteur.<br/><br/>
             Ce mandat exclusif n’emporte aucune cession de droits : l’auteure demeure seule titulaire et responsable de ses créations et de ses droits.</p>
             <h1 className="text-2xl font-SFBold mb-4 ">Hébergement</h1>
             <p className="font-SF mb-4">Le site est hébergé par Vercel Inc., 340 S Lemon Ave #4133, Walnut, CA 91789, USA.<br/>Site : https://vercel.com
@@ -48,7 +48,7 @@ function MentionLegale() {
             <h1 className="text-2xl font-SFBold mb-4 ">Crédits</h1>
             <p className="font-SF mb-4">Conception graphique, direction artistique et contenus : <strong>Alionka HOUL</strong><br/>ISNI : 0000 0005 1675 5683</p>
             <br/>
-            <p>Gestion éditoriale et technique : <strong>Éditions Œuvres — Symbiose Audiovisuelle</strong></p>
+            <p>Gestion éditoriale et technique : <strong>Editions Oeuvres — Symbiose Audiovisuelle</strong></p>
             <br/>
             <p>© 2025 <strong>Alionka HOUL</strong> — Tous droits réservés</p>
         </div>
