d4f8a304a012c494a8298b2b61f7112f29675dfd
alionka34-code <alionka.houl34@gmail.com>
2026-03-06 23:14:32 +0100

    Update Community.jsx

--- frontend/src/pages/Community.jsx
+++ frontend/src/pages/Community.jsx
@@ -16,8 +16,10 @@ function Community() {
         <main className='pt-8 flex flex-col md:flex-row gap-10 mx-4 md:mx-auto md:max-w-6xl'>
         <div className='text-white p-4 border-1 border-gray-300 rounded-lg bg-degrade-marron shadow-lg dark:bg-neutral-800 dark:border-neutral-500'>
             <h1 className='font-SFBold text-2xl'>🔥 Bienvenue dans la Communauté Antifragile !</h1>
-            <p className='font-SF text-xl mt-8'>Tu es au bon endroit. Explore les dossiers à ton rythme, tout ce qu'il te faut est ici.<br/>
-Une question ? Une adresse à partager ? Une recette ? 👉 [Rejoindre le groupe Telegram]</p>
+            <p className='font-SF text-xl mt-8'>Tu es au bon endroit. Voilà comment naviguer :<br/>
+🗂 Les dossiers vidéos : commence par là<br/>
+📄 Les guides : fiches PDF pratiques<br/>
+🔬 Les analyses : pour aller plus loin<br/>Une question ? Une adresse à partager ? Une recette ? 👉 [Rejoindre le groupe Telegram]</p>
         </div>
         <aside className='md:w-2/3 p-4 border-1 border-gray-300 rounded-lg shadow-lg dark:bg-neutral-800 dark:border-neutral-500'>
             <div>
