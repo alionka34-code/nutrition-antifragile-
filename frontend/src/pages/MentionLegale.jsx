cc7c0b623c148d796c10c74c6aed83dbe86696e3
alionka34-code <alionka.houl34@gmail.com>
2026-03-22 11:28:54 +0100

    Update legal mentions in MentionLegale.jsx

--- frontend/src/pages/MentionLegale.jsx
+++ frontend/src/pages/MentionLegale.jsx
@@ -19,34 +19,35 @@ function MentionLegale() {
                 <p className="font-SF">ALIONKA HOUL - NOURRIR AVEC L'INSTINCT<br/>ISSN 3098-7113</p>
             </div>
         <div className="md:mx-40 mx-4 py-8  dark:text-white">
-            <h1 className="text-2xl font-SFBold mb-4 ">Responsable éditoriale et artistique</h1>
+            <h1 className="text-2xl font-SFBold mb-4 ">Auteure, responsable éditoriale et artistique</h1>
             <p className="font-SF mb-4">Alionka HOUL : auteure de texte, œuvre & image<br/>ISNI : 0000 0005 1675 5683<br/>Siège professionnel : Montpellier, France.<br/>Ce site présente les œuvres, publications et actualités professionnelles d’Alionka HOUL
             <br/>Identifiant ARK : ark:/76272/eo-alionka-nourrir-avec-l-instinct/2025</p>
+            <h1 className="text-2xl font-SFBold mb-4 ">Crédits</h1>
+            <p className="font-SF mb-4">© Alionka HOUL</p>
+             <h1 className="text-2xl font-SFBold mb-4 ">Propriété intellectuelle</h1>
+            <p className="font-SF mb-4">L’ensemble des contenus de ce site (textes, images, vidéos, extraits d’œuvres, publications) relève de la propriété exclusive de l’auteure.<br/>Toute reproduction, représentation, adaptation ou diffusion, intégrale ou partielle, sans autorisation écrite préalable, est interdite conformément aux articles L.111-1 et s. du Code de la propriété intellectuelle.</p>
             <h1 className="text-2xl font-SFBold mb-4 ">Gestion administrative et financière</h1>
-            <p className="font-SF mb-4">Les transactions (abonnements, ventes d’ouvrages, dons et services)<br/><br/> Symbiose Audiovisuelle <br/>Association loi 1901 – RNA : W343028438 – SIREN : 891 180 093 <br/>Siège social : Montpellier, France. <br/>Site et contact : https://www.symbiose-audiovisuelle.fr/ <br/><br/>Symbiose Audiovisuelle agit en qualité de structure de gestion, de diffusion et de perception des droits d’auteurs, dans le cadre de conventions et partenariats établis conformément à la législation en vigueur.</p>
+            <p className="font-SF mb-4">Symbiose Audiovisuelle <br/>Association loi 1901 – RNA : W343028438 – SIREN : 891 180 093 <br/>Siège social : Montpellier, France. <br/>Site et contact : https://www.symbiose-audiovisuelle.fr/ <br/><br/>Symbiose Audiovisuelle agit en qualité de structure de gestion, de diffusion et de perception des droits d’auteurs, dans le cadre de conventions et partenariats établis conformément à la législation en vigueur.</p>
+            <h1 className="text-2xl font-SFBold mb-4 ">Éditeur exclusif de l’auteure</h1>
+            <p className="font-SF mb-4">Editions Oeuvres<br/> Marque éditoriale de Symbiose AudiovisuelleL</p>
            <h1 className="text-2xl font-SFBold mb-4 ">Directrice de la publication</h1>
-            <p className="font-SF mb-4"> Suthany HOUL (diffusion, gestion ou la conformité légale)</p>
+            <p className="font-SF mb-4"> Suthany HOUL</p>
             <h1 className="text-2xl font-SFBold mb-4 ">Status de référence</h1>
             <p className="font-SF mb-4">Les statuts applicables sont ceux de l’association Symbiose Audiovisuelle.</p>
-            <p className="font-SF mb-2">👉 Ils sont consultables sur le site officiel de <a href= "https://www.symbiose-audiovisuelle.fr/mentions-l%C3%A9gales" className="text-blue-400">Symbiose Audiovisuelle</a></p>
-            <h1 className="text-2xl font-SFBold mb-4">Gestion, diffusion et distribution</h1>
-            <p className="font-SF mb-4">La gestion administrative, la diffusion et la distribution des œuvres de l’auteure sont assurées, sous mandat exclusif, par Editions Oeuvres — Symbiose Audiovisuelle, dans le cadre de ses statuts et de ses accords de gestion des droits d’auteur.<br/><br/>
-            Ce mandat exclusif n’emporte aucune cession de droits : l’auteure demeure seule titulaire et responsable de ses créations et de ses droits.</p>
+            <p className="font-SF mb-2">Ils sont consultables sur le site officiel de <a href= "https://www.symbiose-audiovisuelle.fr/mentions-l%C3%A9gales" className="text-blue-400">Symbiose Audiovisuelle</a></p>
+    
             <h1 className="text-2xl font-SFBold mb-4 ">Hébergement</h1>
-            <p className="font-SF mb-4">Le site est hébergé par Vercel Inc., 340 S Lemon Ave #4133, Walnut, CA 91789, USA.<br/>Site : https://vercel.com
-            </p>
+            <p className="font-SF mb-4">Le site est hébergé par Vercel Inc.<br/> 440 N Barranca Avenue #4133, Covina, CA 91723, États-Unis.<br/> Site : https://vercel.com<br/>
+
+Pour toute information relative aux données techniques traitées dans le cadre de l'hébergement, il convient de consulter la politique de confidentialité de Vercel Inc. :<br/>  https://vercel.com/legal/privacy-policy</p>
             <h1 className="text-2xl font-SFBold mb-4 ">Données personnelles - RGPD</h1>
-            <p className="font-SF mb-4">Aucune donnée personnelle n’est collectée sans consentement explicite.Les informations recueillies par formulaire (contact, commande, abonnement) sont destinées uniquement à la gestion des échanges et transactions.
+            <p className="font-SF mb-4">Aucune donnée personnelle n’est collectée sans consentement explicite.<br/>Les informations recueillies par formulaire (contact, commande, abonnement) sont destinées uniquement à la gestion des échanges et transactions.
             <br/>    
-            Conformément au Règlement (UE) 2016/679 (RGPD), vous disposez d’un droit d’accès, de rectification et de suppression de vos données personnelles.Toute demande peut être adressée à : rgpd@symbiose-audiovisuelle.fr</p>
+            Conformément au Règlement (UE) 2016/679 (RGPD), vous disposez d’un droit d’accès, de rectification et de suppression de vos données personnelles.Toute demande peut être adressée à :<br/>rgpd@symbiose-audiovisuelle.fr</p>
             <h1 className="text-2xl font-SFBold mb-4 ">Responsabilité</h1>
-            <p className="font-SF mb-4">Les informations diffusées sur ce site sont fournies à titre informatif et artistique.L’éditeur ne saurait être tenu responsable d’erreurs, d’omissions ou de dommages directs ou indirects liés à la consultation ou à l’utilisation du site.</p>
-            <h1 className="text-2xl font-SFBold mb-4 ">Crédits</h1>
-            <p className="font-SF mb-4">Conception graphique, direction artistique et contenus : <strong>Alionka HOUL</strong><br/>ISNI : 0000 0005 1675 5683</p>
-            <br/>
-            <p>Gestion éditoriale et technique : <strong>Editions Oeuvres — Symbiose Audiovisuelle</strong></p>
-            <br/>
-            <p>© 2025 <strong>Alionka HOUL</strong> — Tous droits réservés</p>
+            <p className="font-SF mb-4">Les informations diffusées sur ce site sont fournies à titre informatif et artistique. <br/> Symbiose Audiovisuelle ne saurait être tenue responsable d’erreurs, d’omissions ou de dommages directs ou indirects liés à la consultation ou à l’utilisation du site.</p>
+            
+            <p><strong>© 2025-2026 Alionka HOUL — Tous droits réservés</strong></p>
         </div>
 
         
